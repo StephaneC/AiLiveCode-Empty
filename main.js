346aea9c43cf367a9afbad34698f7b17df786026
Stephane <stephane.castrec@gmail.com>
2017-03-15 17:29:35 +0100

    first commit

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ var myApiKey = "MyAuthenticationTokenIsHereAndIWillFoundABetterLater";
 /** understand JSON in body. */
 app.use(bodyParser.json());
 
+/**
+ * Input test
+ */
+app.get('/time', function(req, res){
+  //check authentication
+  res.send(getTime());
+});
+
 // Endpoint for api.ai
 app.post('/apiwebhook', function(req, res){
 
@@ -18,3 +26,13 @@ app.post('/apiwebhook', function(req, res){
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
+
+
+var getTime = function(cb){
+  var date = new Date();
+  var data = {};
+  var speech =  'Il est ' + (date.getUTCHours() + 1) + ':' + date.getUTCMinutes() + ' et ' + date.getUTCSeconds() + " secondes.";
+  var response = apiHelper.createResponse(speech, speech, data, 'Crédit Mutuel Arkéa');
+  cb(response);
+  return response;
+}
